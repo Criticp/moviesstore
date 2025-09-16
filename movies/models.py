109f67f50d1a8c5020f9e13874fb90f7dfd7fa67
CriticPerfect <avafaeian1@gmail.com>
2025-09-16 14:34:16 -0400

    Hide button

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -11,6 +12,7 @@ class Movie(models.Model):
     def __str__(self):
         return str(self.id) + ' - ' + self.name
 
+
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
     comment = models.CharField(max_length=255)
@@ -20,3 +22,20 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
+
+
+class HiddenMovie(models.Model):
+    """
+    S–Z user story:
+    A per-user toggle that hides a movie from the main listings.
+    """
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('user', 'movie'),)
+
+    def __str__(self):
+        return f"{self.user.username} hides {self.movie.name}"
--- movies/views.py
+++ movies/views.py
@@ -1,63 +1,116 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Movie, Review
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Movie, Review, HiddenMovie
+
 
 def index(request):
+    """
+    Main list of movies.
+    If the user is authenticated, exclude movies they have hidden.
+    Supports ?search= query param as in your original implementation.
+    """
     search_term = request.GET.get('search')
+    qs = Movie.objects.all()
     if search_term:
-        movies = Movie.objects.filter(name__icontains=search_term)
-    else:
-        movies = Movie.objects.all()
+        qs = qs.filter(name__icontains=search_term)
 
-    template_data = {}
-    template_data['title'] = 'Movies'
-    template_data['movies'] = movies
+    if request.user.is_authenticated:
+        qs = qs.exclude(hiddenmovie__user=request.user)
+
+    template_data = {
+        'title': 'Movies',
+        'movies': qs,
+    }
     return render(request, 'movies/index.html', {'template_data': template_data})
 
-def show(request, id):
-    movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie)
 
-    template_data = {}
-    template_data['title'] = movie.name
-    template_data['movie'] = movie
-    template_data['reviews'] = reviews
+def show(request, id):
+    """
+    Movie detail page. Adds `is_hidden` in template_data for the S–Z story.
+    """
+    movie = get_object_or_404(Movie, id=id)
+    reviews = Review.objects.filter(movie=movie).order_by('-date')
+
+    is_hidden = False
+    if request.user.is_authenticated:
+        is_hidden = HiddenMovie.objects.filter(user=request.user, movie=movie).exists()
+
+    template_data = {
+        'title': movie.name,
+        'movie': movie,
+        'reviews': reviews,
+        'is_hidden': is_hidden,
+    }
     return render(request, 'movies/show.html', {'template_data': template_data})
+
 
 @login_required
 def create_review(request, id):
-    if request.method == 'POST' and request.POST['comment'] != '':
-        movie = Movie.objects.get(id=id)
-        review = Review()
-        review.comment = request.POST['comment']
-        review.movie = movie
-        review.user = request.user
-        review.save()
-        return redirect('movies.show', id=id)
-    else:
-        return redirect('movies.show', id=id)
+    movie = get_object_or_404(Movie, id=id)
+    if request.method == 'POST' and request.POST.get('comment', '').strip():
+        Review.objects.create(
+            movie=movie,
+            user=request.user,
+            comment=request.POST['comment'].strip(),
+        )
+        messages.success(request, 'Review added.')
+    return redirect('movies.show', id=id)
+
 
 @login_required
 def edit_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id)
-    if request.user != review.user:
-        return redirect('movies.show', id=id)
-
+    movie = get_object_or_404(Movie, id=id)
+    review = get_object_or_404(Review, id=review_id, user=request.user)
     if request.method == 'GET':
-        template_data = {}
-        template_data['title'] = 'Edit Review'
-        template_data['review'] = review
+        template_data = {
+            'title': 'Edit Review',
+            'movie': movie,
+            'review': review,
+        }
         return render(request, 'movies/edit_review.html', {'template_data': template_data})
-    elif request.method == 'POST' and request.POST['comment'] != '':
-        review = Review.objects.get(id=review_id)
-        review.comment = request.POST['comment']
+    elif request.method == 'POST' and request.POST.get('comment', '').strip():
+        review.comment = request.POST['comment'].strip()
         review.save()
-        return redirect('movies.show', id=id)
-    else:
-        return redirect('movies.show', id=id)
+        messages.success(request, 'Review updated.')
+    return redirect('movies.show', id=id)
+
 
 @login_required
 def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
+    messages.success(request, 'Review deleted.')
     return redirect('movies.show', id=id)
+
+
+# -------------------- S–Z user story: hide/un-hide movies --------------------
+
+@login_required
+def hide_toggle(request, id):
+    """
+    POST: toggle hidden state for a movie for the current user.
+    Redirects back to the referring page (or the index).
+    """
+    movie = get_object_or_404(Movie, id=id)
+    existing = HiddenMovie.objects.filter(user=request.user, movie=movie).first()
+    if existing:
+        existing.delete()
+        messages.info(request, f'"{movie.name}" un-hidden.')
+    else:
+        HiddenMovie.objects.create(user=request.user, movie=movie)
+        messages.info(request, f'"{movie.name}" hidden.')
+    return redirect(request.META.get('HTTP_REFERER') or 'movies.index')
+
+
+@login_required
+def hidden_list(request):
+    """
+    A page showing all movies this user has hidden, with un-hide buttons.
+    """
+    movies = Movie.objects.filter(hiddenmovie__user=request.user).order_by('name')
+    template_data = {
+        'title': 'Hidden Movies',
+        'movies': movies,
+    }
+    return render(request, 'movies/hidden_list.html', {'template_data': template_data})
