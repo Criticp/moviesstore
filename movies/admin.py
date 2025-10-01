ba269399933259e2de03ae2ac6d69462267bd4fc
CriticPerfect <avafaeian1@gmail.com>
2025-10-01 00:50:46 -0400

    Admin View of Petitions
    Adding admin view of the petitions and the actual votes for the petitions.

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Movie, Review
+from .models import Movie, Review, Petition, PetitionVote
+
 
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
@@ -7,3 +8,18 @@ class MovieAdmin(admin.ModelAdmin):
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
+
+@admin.register(Petition)
+class PetitionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "requested_by", "created_at")
+    search_fields = ("title", "requested_by__username", "reason")
+    list_filter = ("created_at",)
+    list_display_links = ("id", "title")
+
+
+@admin.register(PetitionVote)
+class PetitionVoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "petition", "user", "created_at")
+    search_fields = ("petition__title", "user__username")
+    list_filter = ("created_at",)
+    list_display_links = ("id", "petition")
